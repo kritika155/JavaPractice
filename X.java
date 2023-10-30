372afc63545d416384321b89e4469186bc226e39
Kritika Roy <roykritika00@gmail.com>
2023-10-30 14:25:35 +0530

    ascending order java

---
+++ X.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class X{
+	public static void B(int i){
+		System.out.println(i*2);
+	}
+	public static void main(String[] args){
+		Scanner sc=new Scanner(System.in);
+		int i=sc.nextInt();
+		B(i);
+	}
+}
